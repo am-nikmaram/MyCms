b79efa8109769e4dd49930691fc3c7c67bd3ef71
amir nikmaram <am.nikmaram@hotmail.com>
2019-09-25 09:52:31 +0330

    session 2 after models

---
+++ MyCms.DomainClasses/PageGroup/PageGroup.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyCms.DomainClasses.PageGroup
+{
+    public class PageGroup
+    {
+        public PageGroup()
+        {
+
+        }
+
+        [Key]
+        public int GroupID { get; set; }
+        [Display(Name ="عنوان گروه")]
+        [Required(ErrorMessage ="لطفاً {0} را وارد کنید")]
+        [MaxLength(200)]
+        public string GroupTitle { get; set; }
+
+
+        public virtual List<Page.Page> Pages { get; set; }
+        
+    }
+}
