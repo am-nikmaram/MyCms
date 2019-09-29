e0f163c4aaa6f56b17df12c2aa067be04682dc64
amir nikmaram <am.nikmaram@hotmail.com>
2019-09-29 15:31:41 +0330

    repository 2

--- MyCms.Services/Repositories/IPageGroupRepository.cs
+++ MyCms.Services/Repositories/IPageGroupRepository.cs
@@ -1,10 +1,20 @@
-﻿using System;
+﻿using MyCms.DomainClasses.PageGroup;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace MyCms.Services.Repositories
 {
-    interface IPageGroupRepository
+    public interface IPageGroupRepository
     {
+        List<PageGroup> GetAllPageGroups();
+        PageGroup GetPageGroupById(int groupid);
+        int InsertPageGroup(PageGroup pageGroup);
+        void UpdatePageGroup(PageGroup pageGroup);
+        void DeletePageGroup(int groupId);
+        void DeletePageGroup(PageGroup pageGroup);
+   
+
+               
     }
 }
---
+++ MyCms.Services/Services/PageGroupRepository.cs
@@ -0,0 +1,50 @@
+﻿using MyCms.DataLayer.Context;
+using MyCms.DomainClasses.PageGroup;
+using MyCms.Services.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyCms.Services.Services
+{
+    public class PageGroupRepository : IPageGroupRepository
+    {
+        private MyCmsDbContext _db;
+
+        public PageGroupRepository(MyCmsDbContext db)
+        {
+            this._db = db; 
+
+        }
+
+        public void DeletePageGroup(int groupId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeletePageGroup(PageGroup pageGroup)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<PageGroup> GetAllPageGroups()
+        {
+            throw new NotImplementedException();
+        }
+
+        public PageGroup GetPageGroupById(int groupid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int InsertPageGroup(PageGroup pageGroup)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdatePageGroup(PageGroup pageGroup)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
